21f5b272760ecdee06cf9dc77080865616672808
siddhant <siddhanjain935@gmail.com>
2022-07-06 02:36:20 +0530

    final

--- com/mindarray/nms/winrm/Discovery.go
+++ com/mindarray/nms/winrm/Discovery.go
@@ -26,7 +26,8 @@ func Discovery(credentials map[string]interface{}) {
 		if userReadableError {
 			errorOccurred = append(errorOccurred, "wrong ip or port ( connection refused )")
 		} else {
-			errorOccurred = append(errorOccurred, "wrong username or password ( unable to authenticate )")
+			//	errorOccurred = append(errorOccurred, "wrong username or password ( unable to authenticate )")
+			errorOccurred = append(errorOccurred, err2.Error())
 		}
 		credentials["status"] = "fail"
 	}
@@ -34,7 +35,37 @@ func Discovery(credentials map[string]interface{}) {
 		credentials["status"] = "success"
 		a := "aa"
 		output := ""
-		cmd := "hostname"
+		//cmd := "hostname"
+		cmd := "(Get-Counter '\\Processor(_Total)\\% Processor Time'," +
+			"'\\Processor(_Total)\\% User Time'," +
+			"'\\Processor(_Total)\\% Interrupt Time'," +
+			"'\\Processor(_Total)\\% Idle Time'," +
+			"'\\System\\Processor Queue Length'," +
+			"'\\Memory\\Available MBytes'," +
+			"'\\Memory\\Committed Bytes'," +
+			"'\\Memory\\Commit Limit'," +
+			"'\\LogicalDisk(*)\\Disk Write Bytes/sec'," +
+			"'\\LogicalDisk(*)\\Disk Read Bytes/sec'," +
+			"'\\LogicalDisk(*)\\Disk Reads/sec'," +
+			"'\\LogicalDisk(*)\\Disk Writes/sec'," +
+			"'\\LogicalDisk(*)\\% Disk Read Time'," +
+			"'\\LogicalDisk(*)\\% Disk Write Time'," +
+			"'\\LogicalDisk(*)\\% Disk Time'," +
+			"'\\LogicalDisk(*)\\Avg. Disk Queue Length'," +
+			"'\\Network Interface(*)\\Bytes Received/sec'," +
+			"'\\Network Interface(*)\\Output Queue Length'," +
+			"'\\Network Interface(*)\\Bytes Sent/sec'," +
+			"'\\Network Interface(*)\\Bytes Total/sec'," +
+			"'\\Network Interface(*)\\Packets Received/sec'," +
+			"'\\Network Interface(*)\\Packets Sent/sec'," +
+			"'\\System\\System Up Time','\\System\\Processes'," +
+			"'\\System\\Threads','\\Memory\\Pages/sec'," +
+			"'\\Memory\\Page Faults/sec'," +
+			"'\\Processor(_Total)\\Interrupts/sec'," +
+			"'\\System\\Context Switches/sec'," +
+			"'\\Memory\\Free & Zero Page List Bytes'," +
+			"'\\Memory\\Pool Paged Bytes'," +
+			"'\\Memory\\Pool Nonpaged Bytes' -ErrorAction SilentlyContinue).countersamples | Format-List  -Property Path,Cookedvalue"
 		output, _, _, err = client.RunPSWithString(cmd, a)
 		if err != nil {
 			credentials["status"] = "fail"
